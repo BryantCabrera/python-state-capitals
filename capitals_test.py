6b728d6394f9c49715b13d59630d243e61988a59
Bryant <cabrera.bryant@gmail.com>
2019-02-19 11:10:58 -0800

    Adds random.shuffle to create a new array of randomized states.

--- capitals_test.py
+++ capitals_test.py
@@ -12,10 +12,16 @@ states = [
 
 
 print('~~~~~~~~~~~~~~~~~~~~ \n Hello player, welcome to Bryant Cabrera\'s State Capitals Game. \n In your terminal, you will be prompted to type the capital of the state named.  Answers are case sensitive. \n For every correct answer, your correct counter goes up by 1. \n For every incorrect answer, your incorrect counter goes up by 1. \n Once all states have been cycled through, you will get a tally of how many you got correct and how many you got incorrect. \n Try to improve your score until you get all 50 correct! \n ~~~~~~~~~~~~~~~~~~~~ ')
+
+from random import shuffle
+# while len(states) > 0:
+#     random_number = random.randint(0, len(states))
+#     print(states[random_number])
+#     states.remove(states[random_number])
+
 # for i in range(0, len(states)):
 #     print(states[i]['name'])
-import random
-while len(states) > 0:
-    random_number = random.randint(0, len(states))
-    print(states[random_number])
-    states.remove(states[random_number])
+randomized_states = [[states[i]] for i in range(0, len(states))]
+shuffle(randomized_states)
+
+print(randomized_states)
